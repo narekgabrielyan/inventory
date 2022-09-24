470e4eeffb74b00f6445edf49cbd1e61a30ab015
narek.gabrielyan <narekgabrielyan998@gmail.com>
2022-09-24 05:11:20 +0400

    docs: fake data created

---
+++ src/utils/fakeData.ts
@@ -0,0 +1,28 @@
+import {getUId} from "./helpers";
+
+export const suppliesData = [
+    {
+        id: getUId(),
+        name: 'bag'
+    },
+    {
+        id: getUId(),
+        name: 'bottle'
+    },
+    {
+        id: getUId(),
+        name: 'knife'
+    },
+    {
+        id: getUId(),
+        name: 'map'
+    },
+    {
+        id: getUId(),
+        name: 'compass'
+    },
+    {
+        id: getUId(),
+        name: 'lamp'
+    }
+]
